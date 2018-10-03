619c2b9e0d5d4d428aea0813e255af0d2691037d
tboychuk <tarass.boychuk@gmail.com>
2018-10-03 15:51:39 +0300

    Complete the task "Hello JPA Repository"

--- hello-jpa-repository/src/main/java/com/bobocode/config/JpaConfig.java
+++ hello-jpa-repository/src/main/java/com/bobocode/config/JpaConfig.java
@@ -1,9 +1,14 @@
 package com.bobocode.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
 
@@ -17,24 +22,36 @@ import javax.sql.DataSource;
  * todo: 3. Configure {@link javax.persistence.EntityManagerFactory} bean with name "entityManagerFactory"
  * todo: 4. Enable JPA repository, set appropriate package using annotation property "basePackages"
  */
+@Configuration
+@EnableJpaRepositories(basePackages = "com.bobocode.dao")
 public class JpaConfig {
 
+    @Bean
     public DataSource dataSource() {
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
                 .build();
     }
 
+    @Bean
     public JpaVendorAdapter jpaVendorAdapter() {
         // todo: create HibernateJpaVendorAdapter
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
         // todo: set H2 database
+        adapter.setDatabase(Database.H2);
         // todo: enable DDL generation
-        throw new UnsupportedOperationException("Application won't start until you provide configs");
+        adapter.setGenerateDdl(true);
+        return adapter;
     }
 
-    public LocalContainerEntityManagerFactoryBean localContainerEMF() {
+    @Bean("entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean localContainerEMF(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
         // todo: create and configure required bean
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setJpaVendorAdapter(jpaVendorAdapter);
         // todo: set package "com.bobocode.model" to scan for JPA entities
-        throw new UnsupportedOperationException("Application won't start until you provide configs");
+        emf.setPackagesToScan("com.bobocode.model");// JPA entity classes will be loaded from this package
+        return emf;
     }
 }
--- hello-jpa-repository/src/main/java/com/bobocode/config/RootConfig.java
+++ hello-jpa-repository/src/main/java/com/bobocode/config/RootConfig.java
@@ -1,6 +1,13 @@
 package com.bobocode.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
 
 /**
  * This class is provides root Java config for Spring application.
@@ -10,6 +17,14 @@ import org.springframework.transaction.PlatformTransactionManager;
  * todo: 2. Configure {@link PlatformTransactionManager} bean with name "transactionManager"
  * todo: 3. Enable transaction management
  */
+@Configuration
+@ComponentScan("com.bobocode")
+@EnableTransactionManagement
 public class RootConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 }
 
